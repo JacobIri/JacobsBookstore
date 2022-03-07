eba0e3e1c2b0dbf27c7d7067701f027a1453bc59
JacobIri <76026530+JacobIri@users.noreply.github.com>
2022-03-07 15:05:46 -0700

    Test

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,9 @@ namespace JacobsBookstore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //ADDED BY JACOB
+            services.AddControllersWithViews();
+            //
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +29,17 @@ namespace JacobsBookstore
                 app.UseDeveloperExceptionPage();
             }
 
+            //ADDED BY JACOB -Can use wwwroot
+            app.UseStaticFiles();
+            //
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //REMOVED THEN REPLACED BY JACOB
+                endpoints.MapDefaultControllerRoute();
+                //
             });
         }
     }
