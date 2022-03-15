bca1d82a041166a123c977fa904f45feee1a8bb1
JacobIri <76026530+JacobIri@users.noreply.github.com>
2022-03-14 20:17:15 -0600

    Fixed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,20 +18,25 @@ namespace JacobsBookstore.Controllers
 
 
 
-        public IActionResult Index()
+        public IActionResult Index(int pageNum = 0) //Why page 1 not page 0   //Page is specified word by asp.net
         {
-            var meh = repo.Books.ToList(); //BookstoreContext line 23
+            int pageSize = 5;
+
+            var meh = repo.Books
+                .OrderBy(b => b.Title)
+                //.Skip(pageNum*pageSize)
+                //.Take(5);              //References BookstoreContext.cs line 23
             return View(meh);
         }
         //SAME AS ABOVE
         //public IActionResult Index() => View();
-        
+
 
 
         //OLD
+
         //private BookstoreContext context { get; set; }
 
-        
         //public HomeController(BookstoreContext temp)
         //{
         //    context = temp;
